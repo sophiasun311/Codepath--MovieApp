405216378cdf95e53c1b6237f1d32d1d0c6335c3
Sophia <qyoung@ucsd.edu>
2019-02-12 19:40:33 -0800

    Initial Commit

---
+++ IOS_MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_MovieApp
+//
+//  Created by Sophia Sun on 2/12/19.
+//  Copyright © 2019 Sophia Qi Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
