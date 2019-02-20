92bddfc800817791964fcb72f7176e788a0659e6
Sophia <qyoung@ucsd.edu>
2019-02-19 20:01:02 -0800

    added detailed page and grid view for superhero tab

---
+++ IOS_MovieApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  IOS_MovieApp
+//
+//  Created by Sophia Sun on 2/19/19.
+//  Copyright © 2019 Sophia Qi Young. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
